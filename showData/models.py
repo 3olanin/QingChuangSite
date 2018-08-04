f0cf5ee60c8fa14361643e4205189a37f6683f49
3olanin <178139945@qq.com>
2018-08-04 17:44:24 +0800

    ready to use

--- showData/models.py
+++ showData/models.py
@@ -3,20 +3,24 @@ from django.db import models
 # Create your models here.
 class FileTxt(models.Model):
 	fileName = models.CharField(max_length=255)
+	store_time = models.BigIntegerField()
 	t_identifier = models.CharField(max_length=255)
 	t_time = models.CharField(max_length=255)
+	t_time_stamp = models.BigIntegerField()
 	def __str__(self):
 		return self.fileName
 
-
 class LineTxt(models.Model):
 	file = models.ForeignKey(FileTxt, on_delete=models.CASCADE)
+	store_time = models.BigIntegerField()
 	t_identifier = models.CharField(max_length=255)
 	t_time = models.CharField(max_length=255)
+	t_time_stamp = models.BigIntegerField()
 	c_imsi = models.CharField(max_length=255)
 	c_tmsi = models.CharField(max_length=255)
 	c_fcn = models.CharField(max_length=255)
 	c_time = models.CharField(max_length=255)
+	c_time_stamp = models.BigIntegerField() 
 	c_lon = models.CharField(max_length=255)
 	c_lat = models.CharField(max_length=255)
 
@@ -27,6 +31,7 @@ class FilePns(models.Model):
 	t_mac = models.CharField(max_length=255)
 	t_ip = models.CharField(max_length=255)
 	t_date = models.CharField(max_length=255)
+	t_date_stamp = models.BigIntegerField()
 	def __str__(self):
 		return self.fileName
 
@@ -36,10 +41,12 @@ class LinePns(models.Model):
 	t_mac = models.CharField(max_length=255)
 	t_ip = models.CharField(max_length=255)
 	t_date = models.CharField(max_length=255)
+	t_date_stamp = models.BigIntegerField()
 	c_mac = models.CharField(max_length=255)
 	c_imei = models.CharField(max_length=255)
 	c_imsi = models.CharField(max_length=255)
 	c_date = models.CharField(max_length=255)
+	c_date_stamp = models.BigIntegerField()
 	c_rsrp = models.CharField(max_length=255)
 	c_lon = models.CharField(max_length=255)
 	c_lat = models.CharField(max_length=255)
--- showData/views.py
+++ showData/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from showData.models import LinePns
 import time
 # Create your views here.
 # coding:utf-8
@@ -6,7 +8,7 @@ from django.http import HttpResponse
 import json
  
 def index(request):
-    return HttpResponse(u"欢迎光临 自强学堂!")
+    return HttpResponse(u"welcome!")
 
 def add(request):
 	a = request.GET['a']
@@ -33,7 +35,21 @@ def indexShow(request):
 	return render(request, 'index.html',context)
 
 def showdataIndex(request):
-	return render(request, 'showdataIndex.html')
+	linepns_list = LinePns.objects.all().order_by('c_date_stamp')
+	page = request.GET.get('page',1)
+	paginator = Paginator(linepns_list, 25) # Show 25 contacts per page
+	try:
+		contacts = paginator.page(page)
+	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+		contacts = paginator.page(1)
+	except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+		contacts = paginator.page(paginator.num_pages)
+	for contact in contacts:
+		time_local = time.localtime(contact.c_date_stamp)
+		contact.c_date_stamp= time.strftime("%Y-%m-%d %H:%M:%S",time_local)
+	return render(request, 'showdataIndex.html',  {'contacts': contacts})
 
 def downloadData(request):
 	context = {}
@@ -48,10 +64,18 @@ def downloadData(request):
 	if timeStampStart > timeStampEnd :
 		return render(request, 'downloadFail.html')
 	else :
+		linepns = LinePns.objects.filter(store_time__range = (timeStampStart,timeStampEnd))
 		response = HttpResponse(content_type='text/plain')
 		response['Content-Disposition'] = 'attachment; filename=target.txt'
-		response.write("aa\n")
-		for i in range(1,50000000):
-			response.write("bb")
+		#response.write("aa\n")
+		for line in linepns:
+			response.write(line.c_mac+",")
+			response.write(line.c_imei+",")
+			response.write(line.c_imsi+",")
+			response.write(line.c_date+",")
+			response.write(line.c_rsrp+",")
+			response.write(line.c_lon+",")
+			response.write(line.c_lat+",")
+			response.write(line.c_isp+"\r\n")
 		return response
 		#return render(request, 'downloadSucceed.html', context)
